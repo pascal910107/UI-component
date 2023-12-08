f77520acfebf17987d5854d89cb8dcdc5071c5a2
Pascal Chang <pascalchang@cybersoft4u.com>
2023-12-08 17:55:28 +0800

    temp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,20 @@ const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'custom-purple-bg': '#080B12',
+        'custom-purple-text': '#CFB3E6',
+        'custom-purple-border-grd1': '#6C6499',
+        'custom-purple-border-grd2': '#111222',
+        'custom-purple-card-grd1': '#1F193D',
+        'custom-purple-card-grd2': '#05060D',
+        'custom-purple-radial-center': 'rgba(173,155,255,0.3)',
+        'custom-purple-radial-edge': 'rgba(11,12,22,0.3)',
+        'custom-purple-shadow': 'rgba(173,155,255,0.5)',
+      },
       boxShadow: {
         neon: "0 0 5px theme('colors.purple.200'), 0 0 20px theme('colors.purple.700')"
       }
